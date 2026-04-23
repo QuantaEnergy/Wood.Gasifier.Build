16b325ee2e39c996968f9b1889e677a7966d72ee
Your Name <you@example.com>
2026-04-23 23:51:07 +0100

    module

--- From:/Propane Tank/Flange./Dimensions/Octagon/module/trig.py
+++ From:/Propane Tank/Flange./Dimensions/Octagon/module/trig.py
@@ -2,21 +2,27 @@ import math
 from module.var import *
 from module.octagon import *
 
-cosine = math.cos(math.radians(half_angle))
-
-print("Cosine", cosine)
-
-adjacent = math.cos(math.radians(half_angle)) * radius
-
-print("Adjacent", adjacent)
 
 opposite = math.sin(math.radians(half_angle)) * radius
 half_octagon_side = opposite
 
-print("Half side", half_octagon_side)
+octagon_side = 2 * half_octagon_side
 
-print("Radius", radius)
+def test():
+    cosine = math.cos(math.radians(half_angle))
 
-hypotenuse = math.sqrt(adjacent**2 + opposite**2)
+    print("Cosine", cosine)
 
-print("Check work", hypotenuse) 
+    adjacent = math.cos(math.radians(half_angle)) * radius
+
+    print("Adjacent", adjacent)
+
+    print("Half side", half_octagon_side)
+
+    print("Radius", radius)
+
+    hypotenuse = math.sqrt(adjacent**2 + opposite**2)
+
+    print("Check work", hypotenuse) 
+
+#test()
